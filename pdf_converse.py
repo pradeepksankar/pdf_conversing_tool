9e2172cd9ba01ca3467bb328d2e5bf1fd02498a9
pradeepksankar <pradeep.theunsuspectablevictim@gmail.com>
2023-12-08 22:58:43 +0000

    second commit

---
+++ pdf_converse.py
@@ -0,0 +1,65 @@
+from dotenv import load_dotenv
+import os
+from PyPDF2 import PdfReader
+import streamlit as st
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
+# Load environment variables
+load_dotenv()
+
+def process_text(text):
+    # Split the text into chunks using langchain
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=500,
+        chunk_overlap=100,
+        length_function=len
+    )
+    blocks = text_splitter.split_text(text)
+    
+    # Convert the chunks of text into embeddings to form a knowledge base
+    embeddings = OpenAIEmbeddings()
+    knowledgeBase = FAISS.from_texts(blocks, embeddings)
+    
+    return knowledgeBase
+
+def main():
+    st.title("PDF Conversation")
+    
+    pdf = st.file_uploader('Upload PDF:', type='pdf')
+    
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        # Text variable will store the pdf text
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        
+        # Create the knowledge base object
+        knowledgeBase = process_text(text)
+        
+        query = st.text_input('Please initiate conversation with PDF')
+        cancel_button = st.button('Cancel')
+        
+        if cancel_button:
+            st.stop()
+        
+        if query:
+            docs = knowledgeBase.similarity_search(query)
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type='refine')
+            
+            with get_openai_callback() as cost:
+                response = chain.run(input_documents=docs, question=query)
+                print(cost)
+                
+            st.write(response)
+            
+            
+if __name__ == "__main__":
+    main()
